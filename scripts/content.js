07fbb96baae5a129fa50aa5504edf6f8befb7bfe
banflam <rishiagrawal2002@gmail.com>
2025-07-25 14:40:44 -0400

    add script for rendering the reading time

---
+++ scripts/content.js
@@ -0,0 +1,28 @@
+function renderReadingTime(article) {
+  // If no article, then don't render anything
+  if (!article) {
+    return;
+  }
+
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // regex
+  const words = text.matchAll(wordMatchRegExp);
+
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+
+  // Use the same styling as the publish information in the article's header
+
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+renderReadingTime(document.querySelector("article"));
